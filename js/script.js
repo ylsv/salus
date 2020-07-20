c5c8a72be3622bdf7880382abc5afbf2c8c26a36
ylsv <lisov1988@gmail.com>
2020-07-20 19:04:07 +0300

    add carousel instead of many images

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
-let images = document.querySelectorAll('.pics__img');
-images.forEach((img) => {
-	img.addEventListener('click', showFullScreen);
-});
+// let images = document.querySelectorAll('.pics__img');
+// images.forEach((img) => {
+// 	img.addEventListener('click', showFullScreen);
+// });
 
-document.querySelector('.pics').addEventListener('click', showAllImages);
+// document.querySelector('.pics').addEventListener('click', showAllImages);
 
-function showFullScreen(e) {
-	images.forEach((img) => {
-		img.style.display = 'none';
-	});
-	if (e.target.classList.contains('pics__img')) {
-		e.target.style.display = 'inline';
-		e.target.classList.add('large-pic');
-		e.stopPropagation();
-	} else {
-		e.target.parentElement.style.display = 'inline';
-		e.target.parentElement.classList.add('large-pic');
-		e.stopPropagation();
-	}
-}
+// function showFullScreen(e) {
+// 	images.forEach((img) => {
+// 		img.style.display = 'none';
+// 	});
+// 	if (e.target.classList.contains('pics__img')) {
+// 		e.target.style.display = 'inline';
+// 		e.target.classList.add('large-pic');
+// 		e.stopPropagation();
+// 	} else {
+// 		e.target.parentElement.style.display = 'inline';
+// 		e.target.parentElement.classList.add('large-pic');
+// 		e.stopPropagation();
+// 	}
+// }
 
-function showAllImages(e) {
-	images.forEach((img) => {
-		img.style.display = 'inline';
-		img.classList.remove('large-pic');
-	});
-}
+// function showAllImages(e) {
+// 	images.forEach((img) => {
+// 		img.style.display = 'inline';
+// 		img.classList.remove('large-pic');
+// 	});
+// }
+
+// disable scripts for pics because carousel was added
